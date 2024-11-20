54f06f2a8f209d368626884c4ac6c38440700e05
Roman Mezentsev <rsmezentsev@outlook.com>
2024-11-20 21:52:24 +0100

    # This is a combination of 88 commits.
    # This is the commit message #56:
    
    env fix
    
    # This is the commit message #58:
    
    Update bot.tpl
    
    increased resources
    # This is the commit message #60:
    
    Update bot.tpl
    # This is the commit message #61:
    
    fix
    
    # This is the commit message #63:
    
    fix typo
    # This is the commit message #64:
    
    fix config cd
    # This is the commit message #65:
    
    fix
    # This is the commit message #69:
    
    changed build and push
    # This is the commit message #87:
    
    fixes
    
    # This is the commit message #88:
    
    changed version

--- main.py
+++ main.py
@@ -388,12 +388,12 @@ def get_weather(message):
     func for get weather and sending it to user
     """
     if re.match(
-        "glory to ukraine|slava ukraine|слава украине|cлава україні",
+        "version",
         str(message.text).lower(),
     ):
         bot.reply_to(
             message,
-            "Героям Слава!\U0001f1fa\U0001f1e6\nGlory to the Heroes!\U0001f1fa\U0001f1e6",
+            "v1.02",
         )
 
     else:
